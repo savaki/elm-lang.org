5d798b9d001c27cd3f28798c06e80861b14df563
savaki <matt.ho@gmail.com>
2015-06-24 22:31:21 -0700

    added sudo for rpm -i

--- setup-snap-ci.sh
+++ setup-snap-ci.sh
@@ -8,7 +8,7 @@ sudo yum install -y tar wget
 # install the justhub-release rpm
 #
 wget http://sherkin.justhub.org/el6/RPMS/x86_64/justhub-release-2.0-4.0.el6.x86_64.rpm
-rpm -ivh justhub-release-2.0-4.0.el6.x86_64.rpm
+sudo rpm -ivh justhub-release-2.0-4.0.el6.x86_64.rpm
 
 # install haskell
 #

e720c2d14858ea0300596bfa1ccf5f1e161cd79f
savaki <matt.ho@gmail.com>
2015-06-26 10:43:36 -0700

    added artifacts

--- setup-circleci.sh
+++ setup-circleci.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# build the elm website as per instructions 
+#
+#   https://github.com/elm-lang/elm-lang.org
+#
+
 cd Elm-Platform/master
 cp ../../* .
 cabal sandbox init --sandbox ..
